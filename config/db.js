67b869b544a41982021fe68904ac91fdf9f64869
Saurabh Kumar <er.saurabh062@gmail.com>
2025-01-18 18:29:27 +0530

    mongodb

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config(); // Make sure dotenv is required
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to MongoDB", error);
+    console.error(`Error connecting to MongoDB: ${error.message}`);
     process.exit(1);
   }
 };
