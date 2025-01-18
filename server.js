d6c556d36fce03dc8c1175d701333f9999dcfbe0
Saurabh Kumar <er.saurabh062@gmail.com>
2025-01-18 15:04:40 +0530

    added test route

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("uploads"));
+// Example API route
+app.get("/api", (req, res) => {
+  res.send("API is working!");
+});
 
 app.use("/api", userRoutes);
 
